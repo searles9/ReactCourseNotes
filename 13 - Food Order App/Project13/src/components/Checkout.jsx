9b23f1c8d250deaa18943ec20529bfe267921921
searles9 <donovansearles@gmail.com>
2024-01-01 16:27:15 -0500

    updates

--- 13 - Food Order App/Project13/src/components/Checkout.jsx
+++ 13 - Food Order App/Project13/src/components/Checkout.jsx
@@ -4,11 +4,11 @@ import CartContext from "../store/CartContext.jsx";
 import { currencyFormatter } from "../util/formatting.js";
 import Input from "./UI/Input.jsx";
 import Button from "./UI/Button.jsx";
-import UserProgressContext from '../store/UserProgressContext.jsx'
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
-  const userProgressCtx = useContext(UserProgressContext)
+  const userProgressCtx = useContext(UserProgressContext);
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
@@ -16,16 +16,37 @@ export default function Checkout() {
   );
 
   function handleClose() {
-    userProgressCtx.hideCheckout()
+    userProgressCtx.hideCheckout();
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    // send to the backend
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
   }
 
   return (
-    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
-      <form>
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-Mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
@@ -34,7 +55,9 @@ export default function Checkout() {
         </div>
 
         <p className="modal-actions">
-          <Button type="button" onClick={handleClose} textOnly>Close</Button>
+          <Button type="button" onClick={handleClose} textOnly>
+            Close
+          </Button>
           <Button>Submit Order</Button>
         </p>
       </form>
