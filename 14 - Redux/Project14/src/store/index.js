96c192bcf09250f6bbdbebbd4c1ac8681e27f34c
searles9 <donovansearles@gmail.com>
2024-01-02 22:45:42 -0500

    updates

--- 14 - Redux/Project14/src/store/index.js
+++ 14 - Redux/Project14/src/store/index.js
@@ -1,16 +1,24 @@
-import { createStore } from 'redux'
+import { createStore } from "redux";
 
-const counterReducer = (state = {counter: 0}, action) => {
-    switch (action.type) {
-        case 'increment':
-          return { ...state, counter: state.counter + 1 };
-    
-        case 'decrement':
-          return { ...state, counter: state.counter - 1 };
-    
-        default:
-          return state;
-      }
+const initialState =  { counter: 0, showCounter: true }
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "increment":
+      return { counter: state.counter + 1, };
+
+    case "decrement":
+      return { ...state, counter: state.counter - 1 };
+
+    case "increase":
+      return { ...state, counter: state.counter + action.amount };
+
+    case "toggle":
+      return { ...state, showCounter: !state.showCounter };
+
+    default:
+      return state;
+  }
 };
 
 const store = createStore(counterReducer);
