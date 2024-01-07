c289670a5a92fd655a5030311b0791c811623c50
searles9 <donovansearles@gmail.com>
2024-01-06 21:55:36 -0500

    updates

--- 18 - Advanced React Router/Project18/frontend/src/pages/Error.js
+++ 18 - Advanced React Router/Project18/frontend/src/pages/Error.js
@@ -1,9 +1,28 @@
+import MainNavigation from "../components/MainNavigation";
 import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
 
-export default function ErrorPage () {
-    return (
-        <PageContent title="An error occured">
-            <p>Something went wrong!</p>
-        </PageContent>
-    )
+export default function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occured!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = error.data.message
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
--- 18 - Advanced React Router/Project18/frontend/src/pages/Events.js
+++ 18 - Advanced React Router/Project18/frontend/src/pages/Events.js
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
@@ -20,7 +18,8 @@ export async function loader() {
   if (!response.ok) {
     console.log("error");
     //return {isError: true, message: 'Could not fetch events.'}
-    throw {message: 'Could not fetch events.'}
+    //throw new Response(JSON.stringify({message: 'Could not fetch events.'}), {status: 500})
+    return json({ message: "Could not fetch events." }, { status: 500 });
   } else {
     const resData = await response.json();
     return resData.events;
